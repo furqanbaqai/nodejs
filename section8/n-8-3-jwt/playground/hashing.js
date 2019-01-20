5f7ec7fecfe29b372518d061164f29b474ac83b4
Furqan Baqai <baqai.furqan@gmail.com>
2019-01-20 22:13:28 +0400

    Adding JWT cocept files

--- section8/n-8-3-jwt/playground/hashing.js
+++ section8/n-8-3-jwt/playground/hashing.js
@@ -1,6 +1,40 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
+// **********************************************
+// Code Snippet: SHA256 test snippet
+/*
+var message = 'I am user number 3';
+var hash = SHA256(message).toString();
+
+console.log(`Message: ${message}`);
+console.log(`Hash: ${hash}`);
+*/
+// **********************************************
+
+// **********************************************
+// Code Snippet: JWT conceptual model
+/*
+var data = {
+  id: 4
+};
+// Note: here 'somesecret' is a salt
+var token = {
+  data, // Un-ecrypted data
+  hash: SHA256(JSON.stringify(data) + 'somesecret').toString() 
+};
+var resultHash = SHA256(JSON.stringify(token.data)+ 'somesecret').toString();
+if (resultHash === token.hash){
+  console.log('data was not changed');
+}else{
+  console.log('data was changed....');
+}
+*/
+// **********************************************
+
+// **********************************************
+// Code Snippet: JWT Library usage
+// Reg: https://jwt.io
 var data = {
   id: 10
 };
@@ -10,6 +44,7 @@ console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
 console.log('decoded', decoded);
+// **********************************************
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
