2cac6c63b820e92353381fbeb5e687c1feaad6c5
Furqan Baqai <baqai.furqan@gmail.com>
2019-07-27 17:54:16 +0400

    Removing old content

--- section6/node-test/spies/app.js
+++
@@ -1,8 +0,0 @@
-var db = require('./db.js');
-
-module.exports.handleSignup = (email, password) => {
-    // Check if email already exist
-    // Note: In ES6 {email: email, password: password} = {email,password}
-    db.saveUser({ email, password});
-    // Send welcome email
-};
